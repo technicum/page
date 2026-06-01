099d4db52f837ff2f7d34a5e6d7d009b28780ddf
technicum <abhi.technicum@gmail.com>
2026-06-01 20:40:11 +0530

    log

--- config/db.js
+++ config/db.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql2/promise')
 require('dotenv').config()
 
+console.log('DB_HOST:', process.env.DB_HOST)
+console.log('DB_USER:', process.env.DB_USER)
+console.log('DB_NAME:', process.env.DB_NAME)
+
 const pool = mysql.createPool({
   host:            process.env.DB_HOST,
   user:            process.env.DB_USER,
